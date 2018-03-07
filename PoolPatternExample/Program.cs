db99c099bc26a45fdb95b69e6dab726898ca62d3
Intezium <Intezium04@gmail.com>
2018-03-07 16:33:38 +0300

    Добавьте файлы проекта.

---
+++ PoolPatternExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
